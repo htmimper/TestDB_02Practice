1851ca5853efe31bb7acefc1885ed28ddef2afd6
htmimper <htmimper@gmail.com>
2025-06-16 22:15:38 +0900

    02_Javaとデータベースの接続（演習)
    ⑫まで対応。

--- TestDB/src/TestUserDAO.java
+++ TestDB/src/TestUserDAO.java
@@ -25,8 +25,98 @@ try{
 		} 
 try{ 
 	con.close() ; 
+	//データベースとの接続を終了させるという意味です。
+	//これをしないとデータベースを接続したまま次の作業が実行されてしまい、メモリに負荷がかかりますので、
+	//終わりには必ず終了をするようにして下さい。
 } catch (SQLException e ) { 
 	e.printStackTrace(); 
 	} 
-} 
 }
+	public void selectAll() { 
+		DBConnector db = new DBConnector(); 
+		Connection con = db.getConnection(); 
+		 String sql ="select * from test_table"; 
+		 try{ 
+			 PreparedStatement ps = con.prepareStatement(sql); 
+			 ResultSet rs=ps.executeQuery(); 
+			 while (rs.next()) { 
+				 System.out.println(rs.getString("user_name")); 
+				 System.out.println(rs.getString("password")); 
+				 } 
+			 }catch (SQLException e ) { 
+				 e.printStackTrace(); 
+				 } 
+		 try{con.close() ; 
+		     }catch (SQLException e ) {
+		    	 e.printStackTrace(); 
+		    	 }
+		 }
+	public void selectByName(String name)
+	{ 
+		DBConnector db = new DBConnector(); 
+		Connection con = db.getConnection(); 
+		String sql ="select * from test_table where user_name=?"; 
+		try { 
+			PreparedStatement ps = con.prepareStatement(sql); 
+			ps.setString(1, name); 
+			ResultSet rs=ps.executeQuery(); 
+			while (rs.next()) { 
+				System.out.println(rs.getString("user_name")); 
+				System.out.println(rs.getString("password")); 
+				} 
+			} catch (SQLException e ) { 
+				e.printStackTrace(); 
+				} 
+		try { 
+			con.close() ; 
+			} catch (SQLException e ) { 
+				e.printStackTrace(); 
+			}
+		}
+			 
+		public void selectByPassword(String password) { 
+			DBConnector db = new DBConnector(); 
+		     Connection con = db.getConnection(); 
+		 
+		     String sql ="select * from test_table where password=?"; 
+		     try { 
+		    	 PreparedStatement ps = con.prepareStatement(sql); 
+		    	 ps.setString (1, password); 
+		    	 ResultSet rs=ps.executeQuery();
+		    	 while (rs.next()) { 
+		         System.out.println(rs.getString("user_name")); 
+		         System.out.println(rs.getString("password")); 
+		       } 
+		     }catch (SQLException e ) { 
+		    	 e.printStackTrace(); } 
+		 try { con.close() ; 
+		     }catch (SQLException e ) { 
+		    	 e.printStackTrace(); 
+		    	 } 
+		 }
+		public void updateUserNameByUserName(String oldName,String newName) { 
+			DBConnector db = new DBConnector(); 
+			Connection con = db.getConnection(); 
+			
+			String sql ="update test_table set user_name=? where user_name=?"; 
+			try { 
+				PreparedStatement ps = con.prepareStatement(sql); 
+				ps.setString(1, newName); 
+				ps.setString (2, oldName); 
+				int i=ps.executeUpdate(); 
+				if (i>0) { 
+					System.out.println(i + "件更新されました"); 
+					}else{ 
+						System.out.println("該当するデータはありませんでした"); 
+						} 
+				}catch (SQLException e ) { 
+					e.printStackTrace(); 
+					}
+			try{ 
+				con.close() ; 
+				}catch (SQLException e ) { 
+					e.printStackTrace(); 
+				}
+			}
+}
+
